8d1934bc7013054dfe4ffd96d4ad875419f12ab2
Saima786-Far <saimafarheen786786@gmail.com>
2024-03-14 01:08:09 +0530

    Basics Program

---
+++ Basic5.java
@@ -0,0 +1,50 @@
+public class Basic5 {
+    public static void main(String[] args) {
+        //for loop
+
+        // for(int i= 0; i<=10 ; i++){
+        //     System.out.print(i+" ");
+        //      System.out.println(i+" ");
+        //      System.out.println(i);
+
+        // }
+
+
+
+        //while loop
+
+        // int i = 0;
+        //  while(i<=10) {
+        //     System.out.println(i);
+        //     i++;
+        //  }
+
+
+
+         // do-while loop
+        //  int i= 0;
+        //  do {
+        //     System.out.println(i);
+        //     i++;
+            
+        //  } while (i<10);
+
+
+        //Test case 1
+        // int i = 12;
+        // while(i<11){
+        //     System.out.println();
+        // }i++;
+        
+
+        //Test case 2
+
+        int i=12;
+        do {
+            System.out.println(i);
+            i++;
+            
+        } while (i<11);
+        
+    }
+}
---
+++ Basic6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Basic6 {
+    public static void main(String[] args) {
+    //   Scanner sc = new Scanner(System.in);
+    //   int n = sc.nextInt();
+    //   int sum = 0;
+    //   for(int i=1 ; i<=n ;i++){
+    //   sum+=i;
+
+    //   }
+    // System.out.println(sum);
+    // }
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=1; i<=10; i++){
+        int result = n *i;
+      System.out.println( n + " * " + i + " = " + result);
+        
+    }
+   ;
+    
+}
+}
