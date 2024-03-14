32f74086f11506e05b58b7df84363d5f47a7fa08
Saima786-Far <saimafarheen786786@gmail.com>
2024-03-14 15:15:47 +0530

    Program Commit

---
+++ BasicPattern0.java
@@ -0,0 +1,20 @@
+public class BasicPattern0 {
+    public static void main(String[] args) {
+
+        //Nested Loop
+        int n = 4;
+        int m = 5;
+        //outer Loop
+        for(int i =1 ;i<=n; i++){
+            //inner Loop
+            for(int j=1; j<=m;j++){
+                
+                System.out.print("*");
+
+            }
+            System.out.println();
+           
+
+        }
+       
+    }}
